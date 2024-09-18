36d2c61d1450a51c4aea6c215763548d4b0fd076
psych0wh1z <ikeagudike@gmail.com>
2024-09-18 18:54:19 +0100

    Rename ERC20 token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract BoredApes is ERC20 {
+contract Token is ERC20 {
     address private _owner;
 
     constructor(

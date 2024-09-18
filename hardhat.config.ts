94e38ae74b13f31a0ba3557844d80e8de524aafb
psych0wh1z <ikeagudike@gmail.com>
2024-09-18 18:55:00 +0100

    Update config files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,45 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
+// import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
-const { vars } = require("hardhat/config");
-const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
-const ACCOUNT_PRIVATE_KEY = vars.get("ACCOUNT_PRIVATE_KEY");
-const LISK_RPC_URL = vars.get("LISK_RPC_URL");
-const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+const ALCHEMY_MAINNET_API_KEY_URL = process.env.ALCHEMY_MAINNET_API_KEY_URL;
 
-const config: HardhatUserConfig = {
+module.exports = {
     solidity: "0.8.24",
     networks: {
-        // for testnet
-        "lisk-sepolia": {
-            url: LISK_RPC_URL!,
-            accounts: [ACCOUNT_PRIVATE_KEY],
-            gasPrice: 1000000000,
-        },
-        "sepolia": {
-            url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-            accounts: [ACCOUNT_PRIVATE_KEY],
-        },
+        hardhat: {
+            forking: {
+                url: ALCHEMY_MAINNET_API_KEY_URL,
+            }
+        }
     },
-    etherscan: {
-        apiKey: {
-            "lisk-sepolia": "123",
-            "sepolia": ETHERSCAN_API_KEY,
-        },
-        customChains: [
-            {
-                network: "lisk-sepolia",
-                chainId: 4202,
-                urls: {
-                    apiURL: "https://sepolia-blockscout.lisk.com/api",
-                    browserURL: "https://sepolia-blockscout.lisk.com/",
-                },
-            },
-        ],
-    },
-    sourcify: {
-        enabled: false,
-    },
+    lockGasLimit: 200000000000,
+    gasPrice: 10000000000,
 };
-
-export default config;
